655fd238084a16bad1950bf44e6a83ded9d1f628
Anup <anup@kunalinfotech.net>
2013-02-28 09:55:29 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spreedemo::Application.config.secret_token = 'fa861be07513d5c419956f27539066d244218564d91d1512375f8ce97eba89d089fa156dfe0a302c8b580c6abcc9223e6d74a812ce7cd35f5943d794bf5d6a19'
